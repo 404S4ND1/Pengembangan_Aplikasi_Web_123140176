3ec61277c38dc208a405b4fb206bd3ad0c651852
RIYAN SANDI PRAYOGA <mrsandip1106@gmail.com>
2025-11-08 10:06:43 +0700

    Delete src directory

--- src/tests/BookFilter.test.jsx
+++
@@ -1,9 +0,0 @@
-import { render, screen, fireEvent } from "@testing-library/react";
-import BookFilter from "../components/BookFilter/BookFilter";
-
-test("Filter dapat diubah", () => {
-  const setFilter = jest.fn();
-  render(<BookFilter filter="all" setFilter={setFilter} search="" setSearch={() => {}} />);
-  fireEvent.change(screen.getByDisplayValue("Semua"), { target: { value: "baca" } });
-  expect(setFilter).toHaveBeenCalledWith("baca");
-});
--- src/tests/useLocalStorage.test.jsx
+++
@@ -1,8 +0,0 @@
-import { renderHook, act } from "@testing-library/react";
-import { useLocalStorage } from "../hooks/useLocalStorage";
-
-test("useLocalStorage menyimpan dan mengambil data", () => {
-  const { result } = renderHook(() => useLocalStorage("test", []));
-  act(() => result.current[1]([{ title: "Test Book" }]));
-  expect(result.current[0][0].title).toBe("Test Book");
-});
